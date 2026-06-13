e0e8c82cd47de174441e3e4b146828f3f8d9dd9e
Charlottechen1101 <charlottechen1101@gmail.com>
2026-06-13 23:39:19 +0800

    final project

---
+++ final project/script.js
@@ -0,0 +1,11 @@
+$(window).ready(function(){
+
+	$("nav>p:first-of-type").on("click",function(){
+
+		$(this).siblings().each(function(index){
+			$(this).slideToggle((index+1)*500);
+			
+
+		});
+	});
+});
